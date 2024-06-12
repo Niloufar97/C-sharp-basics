0ebd4a09327c44386cc93876b384eb20937d6b72
Niloufar <nilooooofar97@gmail.com>
2024-06-12 10:13:43 +0200

    complete rating movie app

--- movie-rating/movie-rating/Program.cs
+++ movie-rating/movie-rating/Program.cs
@@ -10,7 +10,28 @@ namespace movie_rating
     {
         static void Main(string[] args)
         {
-            
+            string[] movies = new string[3] { "movie1", "movie2", "movie3" };
+            List<int> ranks = new List<int>();
+            for (int i = 0; i < movies.Length; i++) {
+                Console.WriteLine("Rate from 1 to 10: " + movies[i]);
+                int rank = Convert.ToInt32(Console.ReadLine());
+                ranks.Add(rank);
+            }
+            double average = Avg(ranks);
+            Console.WriteLine("the average is: ");
+            Console.WriteLine(average);
+            Console.ReadKey();
+
+        }
+        public static double Avg(List<int> userRanks) 
+        {
+            int sum = 0;
+            for (int i = 0;i < userRanks.Count;i++)
+            {
+                sum += userRanks[i];
+            }
+            double avg = (double)sum / userRanks.Count;
+            return avg;
         }
     }
 }

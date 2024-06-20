f405c453a2150de8b5b4838bebcd7894f1ba7056
Niloufar <nilooooofar97@gmail.com>
2024-06-20 14:15:31 +0200

    add manager class

--- oop/oop/Program.cs
+++ oop/oop/Program.cs
@@ -10,6 +10,19 @@ namespace oop
     {
         static void Main(string[] args)
         {
+            Manager manager = new Manager();
+            manager.Name = "Asghar";
+
+            Client cl1 = manager.AddClient("Akbar");
+            cl1.Buy("Iphone 14 pro");
+            Console.WriteLine(cl1.Name);
+            Console.WriteLine(cl1.GetPoints());
+
+            cl1.Buy("Iphone 15");
+            Console.WriteLine(cl1.GetPoints());
+
+            Console.ReadKey();
+
         }
         public class Client
         {
@@ -20,6 +33,18 @@ namespace oop
 
             public void Buy(string product) { 
                 point++;
+            }
+        }
+
+        public class Manager
+        {
+            public string Name;
+
+            public Client AddClient(string clientName)
+            {
+                Client client1 = new Client();
+                client1.Name = clientName;
+                return client1;
             }
         }
     }

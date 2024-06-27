b90db1e5d61b62712a33372a3ab64c226d7f93bc
Niloufar <nilooooofar97@gmail.com>
2024-06-27 10:38:46 +0200

    without try catch

---
+++ ExceptionHandler/ExceptionManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandler
+{
+    public class ExceptionManager
+    {
+        static int[] Numbers = { 1, 8, 12 , 5 };
+
+        public static int GetNumber(int index)
+        {
+            return Numbers[index]; 
+        }
+    }
+}
---
+++ ExceptionHandler/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please insert an index");
+            int index = Convert.ToInt32(Console.ReadLine());
+            int result = ExceptionManager.GetNumber(index);
+            Console.WriteLine($"The sesult is {result}");
+            Console.ReadKey();
+        }
+    }
+}

7b1e64a82fb9a19e80712e95a85fa9cc4a36d782
Niloufar <nilooooofar97@gmail.com>
2024-07-13 15:26:17 +0200

    learn class properties

---
+++ classProperies/CarModel.cs
@@ -0,0 +1,34 @@
+﻿namespace classProperies;
+
+public class CarModel
+{
+    List<string> validColors = ["blue" , "red" , "green"]; 
+    public string? Name { get; set; } = "";
+    public string? Manufacturer;
+    public string color = string.Empty;
+
+    public CarModel(string name, string manu, string color)
+    {
+        Name = name;
+        Manufacturer = manu;
+        Color = color;
+    }
+
+    public string Color {
+        get {
+            return color;        
+        }
+        set
+        {
+            if (validColors.Contains(value))
+            {
+                color = value;
+            }
+            else
+            {
+                color = "Not defined";
+            }
+        }
+    }
+}
+
---
+++ classProperies/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using classProperies;
+
+Console.WriteLine("Hello, World!");
+CarModel car1 = new CarModel("i10" , "Hyundai" , "blue");
+CarModel car2 = new CarModel("i20", "Hyundai", "gray");
+
+Console.WriteLine(car1.color);
+Console.WriteLine(car2.color);
